2363bd47d9e2c5e4cb29d98cf61bda7fb6c306bb
regvinde <ageychik2505@gmail.com>
2024-06-05 00:45:09 +0300

    Signed-off-by: regvinde <ageychik2505@gmail.com>

--- project.c
+++ project.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main (int argc, char **argv)
 {
+	char first[255],last[255];
+	
+	printf("Enter your name: ");
+	fgets(name,255,stdin);
+	printf("Hello World!!");
 
 	printf("Now enter your last name:");
 	return 0;
