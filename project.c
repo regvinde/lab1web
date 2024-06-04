ffbcc31d5aae1ec0c6759970e7914f31b2eafdf4
regvinde <ageychik2505@gmail.com>
2024-06-04 20:13:04 +0300

    Signed-off-by: regvinde <ageychik2505@gmail.com>

--- project.c
+++ project.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 int main (int argc, char **argv)
 {
-	char first[255],last[255];
-
-	printf("Enter your name: ");
-	printf("Hello World!!")
-
-	printf("Now enter your last name:")
-	return 0;
+	print ("Hello world!\n");
+return 0;	
 }
